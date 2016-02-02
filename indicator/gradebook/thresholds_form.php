05f81ef365b5959ce5b4a92cf211686dd56a409e
Danny Liu <dannydotliu@gmail.com>
2016-02-02 12:26:58 +1100

    resolving php/moodle warning

--- indicator/gradebook/thresholds_form.php
+++ indicator/gradebook/thresholds_form.php
@@ -82,7 +82,8 @@ class engagementindicator_gradebook_thresholds_form {
                         'gradeitem_enabled_'.$gradeitem->id, 
                         $gradeitemrowlabel . $gradeitemhint, 
                         get_string('gradeitem_enabled', 'engagementindicator_gradebook', $gradeitemrowlabel), 
-                        array('data-gradeitem_id'=>$gradeitem->id));
+                        array('data-gradeitem_id' => $gradeitem->id,
+                                          'group' => 'group_gradeitem_'.$gradeitem->id));
                     $mform->addHelpButton('gradeitem_enabled_'.$gradeitem->id, 
                         'group_gradeitem', 
                         'engagementindicator_gradebook');
