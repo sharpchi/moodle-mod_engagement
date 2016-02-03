7668bb106f2d6e7b1b029af26915df3d19906de6
Danny Liu <dannydotliu@gmail.com>
2016-02-03 19:35:35 +1100

    better way to detect turnitin submissions

--- indicator/assessment/indicator.class.php
+++ indicator/assessment/indicator.class.php
@@ -118,7 +118,7 @@ class indicator_assessment extends indicator {
                                            JOIN {turnitintool_parts} b ON (e.turnitintoolid = b.turnitintoolid)
                                            JOIN {turnitintool} a ON (a.id = e.turnitintoolid) 
                                           WHERE e.turnitintoolid $insql 
-                                            AND e.submission_status = 'Submission successfully uploaded to Turnitin.'", $params);
+                                            AND e.submission_objectid IS NOT NULL", $params);
 
         foreach ($tiisubs as $s) {
             $submissions[$s->turnitintoolid][$s->userid]['submitted'] = $s->submission_modified;
